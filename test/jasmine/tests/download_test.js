8e66bcc19feaa85fd745470793f530a406ed83f7
Miklós Tusz <mdtusz@gmail.com>
2016-06-03 17:55:25 -0400

    Snapshot: Test all image format downloads

--- test/jasmine/tests/download_test.js
+++ test/jasmine/tests/download_test.js
@@ -44,45 +44,62 @@ describe('Plotly.downloadImage', function() {
     });
 
     it('should create link, remove link, accept options', function(done) {
-        //use MutationObserver to monitor the DOM
-        //for changes
-        //code modeled after
-        //https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-        // select the target node
-        var target = document.body;
-        var domchanges = [];
+        downloadTest(gd, 'jpeg', done);
+    });
 
-        // create an observer instance
-        var observer = new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutation) {
-                domchanges.push(mutation);
-            });
-        });
+    it('should create link, remove link, accept options', function(done) {
+        downloadTest(gd, 'png', done);
+    });
 
-        Plotly.plot(gd, textchartMock.data, textchartMock.layout).then(function(gd) {
-            // start observing dom
-            // configuration of the observer:
-            var config = { childList: true };
+    it('should create link, remove link, accept options', function(done) {
+        downloadTest(gd, 'webp', done);
+    });
 
-            // pass in the target node and observer options
-            observer.observe(target, config);
-
-            return Plotly.downloadImage(gd, {format: 'jpeg', height: 300, width: 300, filename: 'plotly_download'});
-        }).then(function(filename) {
-            // stop observing
-            observer.disconnect();
-            // look for an added and removed link
-            var linkadded = domchanges[domchanges.length - 2].addedNodes[0].outerHTML;
-            var linkdeleted = domchanges[domchanges.length - 1].removedNodes[0].outerHTML;
-
-            // check for a <a element and proper file type
-            expect(linkadded.split('href="')[1].split('jpeg;')[0]).toEqual('data:image/');
-            // check that filename option handled properly
-            expect(filename).toBe('plotly_download.jpeg');
-
-            // check that link removed
-            expect(linkadded).toBe(linkdeleted);
-            done();
-        });
+    it('should create link, remove link, accept options', function(done) {
+        downloadTest(gd, 'svg', done);
     });
 });
+
+
+function downloadTest(gd, format, done) {
+    //use MutationObserver to monitor the DOM
+    //for changes
+    //code modeled after
+    //https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+    // select the target node
+    var target = document.body;
+    var domchanges = [];
+
+    // create an observer instance
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            domchanges.push(mutation);
+        });
+    });
+
+    Plotly.plot(gd, textchartMock.data, textchartMock.layout).then(function(gd) {
+            // start observing dom
+            // configuration of the observer:
+        var config = { childList: true };
+
+        // pass in the target node and observer options
+        observer.observe(target, config);
+
+        return Plotly.downloadImage(gd, {format: format, height: 300, width: 300, filename: 'plotly_download'});
+    }).then(function(filename) {
+        // stop observing
+        observer.disconnect();
+        // look for an added and removed link
+        var linkadded = domchanges[domchanges.length - 2].addedNodes[0].outerHTML;
+        var linkdeleted = domchanges[domchanges.length - 1].removedNodes[0].outerHTML;
+
+        // check for a <a element and proper file type
+        expect(linkadded.split('href="')[1].split(format)[0]).toEqual('data:image/');
+        // check that filename option handled properly
+        expect(filename).toBe('plotly_download.' + format);
+
+        // check that link removed
+        expect(linkadded).toBe(linkdeleted);
+        done();
+    });
+}
